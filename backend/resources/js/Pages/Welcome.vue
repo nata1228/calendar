f78a3e2530ba71f61400c6e5ca9c85188be6649b
Takumi <>
2022-07-11 19:44:44 +0900

    Inertiaをセットアップ

---
+++ backend/resources/js/Pages/Welcome.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { onMounted } from "vue";
+onMounted(() => {
+    console.log("Welcome Page mounted");
+});
+</script>
+<template>
+    <h1>Welcome Inertia.js</h1>
+</template>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,22 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome');
 });
 
-Route::get('/test', function() {
-    return view('test');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/test', function() {
+//     return view('test');
+// });
